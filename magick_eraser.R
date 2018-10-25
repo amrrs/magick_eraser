44d1591702da5de910e4f67ff1fd6f503f0ced3d
amrrs <amrrs@outlook.com>
2018-10-25 22:56:59 +0530

    first commit !!!

---
+++ magick_eraser.R
@@ -0,0 +1,15 @@
+library(magick)
+
+image <- image_read("tweet_linkedin.jpeg")
+
+image %>% 
+ image_channel() %>% 
+  image_modulate(brightness = 350) %>% 
+  image_quantize(max = 80)  %>% 
+  image_modulate(brightness = 450)  -> new
+
+images <- c(image %>% image_annotate("Supposedly Hidden", size = 40, gravity = "South", strokecolor = "red") , 
+            new %>%   image_annotate("magick with R", size = 40, gravity = "South", strokecolor = "green"))
+
+image_append(images)
+
