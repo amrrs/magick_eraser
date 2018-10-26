86e8a7ac701b2f9d3d7ed17e952249ab11f11b4e
amrrs <amrrs@outlook.com>
2018-10-26 14:56:03 +0530

    care to add some comments! 🕶🕶🕶🕶🕶

--- magick_eraser.R
+++ magick_eraser.R
@@ -1,12 +1,21 @@
+
+#loading R's package for Imagemagick
 library(magick)
 
+#reading the input image
 image <- image_read("tweet_linkedin.jpeg")
 
+
+#some simple image manipulation like converting the image to grayscale and then tuning basic parameters
+#save the newly created image as new
 image %>% 
   image_channel() %>% 
   image_modulate(brightness = 350) %>% 
   image_quantize(max = 80)  %>% 
   image_modulate(brightness = 500)  -> new
+
+
+#lets stitch both the image together 
 
 images <- c(image %>% image_annotate("Supposedly Hidden", size = 40, gravity = "South", strokecolor = "red") %>% image_border() , 
             new %>%   image_annotate("magick with R", size = 40, gravity = "South", strokecolor = "green") %>% image_border())
